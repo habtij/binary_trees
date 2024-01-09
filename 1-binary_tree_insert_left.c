a7eeccb1dcb3671c1fba4c60655e16756bb05dff
Habeeb <tijanihabeeb12@outlook.com>
2024-01-09 12:46:35 +0100

    Update: 1. Insert left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -6,10 +6,10 @@
  * @value: The value to store in the new node
  *
  * Return: NULL, if parent is NULL or there is an error.
- * Otherwise, a pointer to the new node
+ *      Otherwise, a pointer to the new node
  *
  * Description: If parent has left-child already, the new node takes its
- * place and the old left-child is set as the left-child of the new node
+ *      place and the old left-child is set as the left-child of the new node
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
